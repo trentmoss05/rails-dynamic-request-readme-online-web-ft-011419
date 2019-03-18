c89361f892606c9bc85c7e304dfcb95ab665a0f5
Trent Moss <trentmoss05@gmail.com>
2019-03-18 19:26:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/:id', to: 'posts#show'
+  resources :posts, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
